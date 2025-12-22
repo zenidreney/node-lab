0b17da6a3cd5cc29b312208b86e77b13e20de464
Deniz <auo@tuta.io>
2025-12-22 16:06:02 +0100

    refactor

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -29,13 +29,13 @@ export async function serveStatic(req, res, baseDir) {
 		console.log(err.code);
 		if (err.code === "ENOENT") {
 			console.log("eno yo");
-			const content = await fs.readFile("/home/zenid/node-lab/public/404.html");
-			const ext = path.extname("/home/zenid/node-lab/public/404.html");
-			console.log(ext);
-			const contentType = getContentType(ext);
+			const content = await fs.readFile(path.join(publicDir, "404.html"));
+			// const ext = path.extname("/home/zenid/node-lab/public/404.html");
+			// console.log(ext);
+			// const contentType = getContentType(ext);
 
-			console.log(contentType);
-			sendResponse(res, 404, contentType, content);
+			// console.log(contentType);
+			sendResponse(res, 404, "text/html", content);
 		} else {
             sendResponse(res, 500, "text/html", `<html><h1>Server Error: ${err.code}</h1></html>`)
         }
